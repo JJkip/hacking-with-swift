8c8e43c998e06f114a58073527b75a3b9f52fa7b
JayJay <jjlangatjose@gmail.com>
2020-01-15 23:58:12 +0300

    Enum associated values

--- 002-Day.playground/Contents.swift
+++ 002-Day.playground/Contents.swift
@@ -72,3 +72,18 @@ enum Result {
 
 let result4 = Result.failure
 
+//Enum associated values
+enum Activity {
+    case bored
+    case running
+    case talking
+    case singing
+}
+
+enum Activity2 {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+let talking = Activity.talking(topic: "football")
