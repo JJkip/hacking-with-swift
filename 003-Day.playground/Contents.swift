2983f3bf2c06bb57738df2e5b1610c8503a28acc
JayJay <jjlangatjose@gmail.com>
2020-01-18 23:58:40 +0300

    Operator overloading

--- 003-Day.playground/Contents.swift
+++ 003-Day.playground/Contents.swift
@@ -12,3 +12,15 @@ let product = firstScore * secondScore
 let divided  = firstScore / secondScore
 
 let remainder = 13 % secondScore
+
+//Operator overloading
+let meaningOfLife = 42
+let doubleMeaning = 42 + 42
+
+let fakers = "Fakers gonna"
+let action = fakers + "fake"
+
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["George", "Ringo"]
+let beatles = firstHalf + secondHalf
+
