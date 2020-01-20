16dd10e6bd6761a94a38f1059c53096b237bd5d1
JayJay <jjlangatjose@gmail.com>
2020-01-20 23:14:23 +0300

    Range operators

--- 003-Day.playground/Contents.swift
+++ 003-Day.playground/Contents.swift
@@ -108,3 +108,15 @@ case "sunny" :
 default:
     print("Enjoy your day!")
 }
+
+//Range operators
+let examScore = 85
+
+switch examScore {
+case 0..<50:
+    print("You failed badly")
+case 50..<85
+    print("You did OK.")
+default:
+    print("You did great")
+}
