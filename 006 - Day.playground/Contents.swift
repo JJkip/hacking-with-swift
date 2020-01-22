853fe24248e5bb36abf55434e48265de69175d39
JayJay <jjlangatjose@gmail.com>
2020-01-22 23:56:26 +0300

    Trailing closure syntax

--- 006 - Day.playground/Contents.swift
+++ 006 - Day.playground/Contents.swift
@@ -29,3 +29,17 @@ func travel(action: () -> Void){
     print("I arrived!")
 }
 travel(action: driving)
+
+//Trailing closure syntax
+func travelling(action: () -> Void){
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+
+travelling() {
+    print("I'm driving in my car")
+}
+travelling {
+    print("I'm driving in my car")
+}
