6aa3b4dc44f3dbb53f9a1582f3f9b44db6c9fc25
JayJay <jjlangatjose@gmail.com>
2020-01-22 23:52:44 +0300

    Closures as parameters

--- 006 - Day.playground/Contents.swift
+++ 006 - Day.playground/Contents.swift
@@ -18,3 +18,14 @@ let drivingWithReturn = { (place: String) -> String in
 }
 let message = drivingWithReturn("London")
 print(message)
+
+//Closures as parameters
+let dring = {
+    print("I'm driving in my car")
+}
+func travel(action: () -> Void){
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+travel(action: driving)
