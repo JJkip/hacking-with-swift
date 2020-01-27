1edf4d375e2f28d7b5b0f5960e3739cf8935b9f0
JayJay <jjlangatjose@gmail.com>
2020-01-28 00:06:46 +0300

    Property observers

--- 007-Day.playground/Contents.swift
+++ 007-Day.playground/Contents.swift
@@ -22,3 +22,17 @@ struct Sport {
 }
 let chessBoxing =  Sport(name: "Chessboxing", isOlympicSport: false)
 print(chessBoxing.olympicStatus)
+
+//Property observers
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet{
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
