3204308228ce36812dc7c6116fec61a52df9b0cb
JayJay <jjlangatjose@gmail.com>
2020-01-27 23:57:13 +0300

    Computed properties

--- 007-Day.playground/Contents.swift
+++ 007-Day.playground/Contents.swift
@@ -1,9 +1,24 @@
 import UIKit
 
 //Creating Structs
-struct Sport {
+struct Sport1 {
     var name: String
 }
-var tennis = Sport(name: "Tennis")
+var tennis = Sport1(name: "Tennis")
 print(tennis.name)
 tennis.name = "Lawn tennis"
+
+//Computed properties
+struct Sport {
+    var name: String
+    var isOlympicSport: Bool
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+let chessBoxing =  Sport(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
