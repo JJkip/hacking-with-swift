a73fae817ea2907de44d30e999f7ddd5246cadd6
JayJay <jjlangatjose@gmail.com>
2020-01-28 00:16:04 +0300

    Methods inside struct

--- 007-Day.playground/Contents.swift
+++ 007-Day.playground/Contents.swift
@@ -36,3 +36,18 @@ var progress = Progress(task: "Loading data", amount: 0)
 progress.amount = 30
 progress.amount = 80
 progress.amount = 100
+
+//:TODO
+//willSet
+
+//Methods or Functins inside Struct
+
+struct City{
+    var population: Int
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+let nairobi = City(population: 2_000_000)
+nairobi.collectTaxes()
+
