d3788d096f23b96c1cabe3469da088a8b0bc4d31
JayJay <jjlangatjose@gmail.com>
2020-02-03 00:59:25 +0300

    Extensions

--- 011-Day.playground/Contents.swift
+++ 011-Day.playground/Contents.swift
@@ -26,3 +26,18 @@ protocol  HasVacation {
 protocol Employee: Payable, NeedsTrainging, HasVacation {
     
 }
+
+//Extensions
+extension Int {
+    func squared() -> Int {
+        return self * self
+    }
+}
+let number = 8
+number.squared()
+
+extension Int {
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+}
