b91d2b59b20e5c41253e3a999240997f522b3c53
JayJay <jjlangatjose@gmail.com>
2020-02-02 22:26:53 +0300

    Protocal

---
+++ 011-Day.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//Protocols
+protocol Identifiable {
+    var id: String { get set }
+}
+
+struct User: Identifiable {
+    var id:String
+}
+
+func displayID(thing: Identifiable){
+    print("My ID is \(thing.id)")
+}
