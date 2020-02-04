9af56137d566c1fbc15e0a798e6cb840fc1654ea
JayJay <jjlangatjose@gmail.com>
2020-02-04 23:56:16 +0300

    Handling missing data

---
+++ 012-Day.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+//Handling missing data
+/*To make a type optional, add a question mark after it.*/
+var age: Int? = nil
+/*That doesn’t hold any number – it holds nothing. But if we later learn that age, we can use it:*/
+age = 38
