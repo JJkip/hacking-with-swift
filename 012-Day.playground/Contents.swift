c684bb6be36ed144686a039643a6c36ca84610e3
JayJay <jjlangatjose@gmail.com>
2020-02-05 23:10:43 +0300

    Implicitly unwrapped optionals

--- 012-Day.playground/Contents.swift
+++ 012-Day.playground/Contents.swift
@@ -30,3 +30,8 @@ let num = Int(str)
 let num2 = Int(str)!
 /*Swift will immediately unwrap the optional and make num a regular Int rather than an Int?. But if you’re wrong – if str was something that couldn’t be converted to an integer – your code will crash.
 As a result, you should force unwrap only when you’re sure it’s safe – there’s a reason it’s often called the crash operator.*/
+
+//Implicitly unwrapped optionals
+/**/
+let age2: Int! = nil
+/*Because they behave as if they were already unwrapped, you don’t need if let or guard let to use implicitly unwrapped optionals. However, if you try to use them and they have no value – if they are nil – your code crashes.*/
