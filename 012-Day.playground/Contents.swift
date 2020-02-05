c205e6508cac0bae937d75413b41cdd7f46f9cb6
JayJay <jjlangatjose@gmail.com>
2020-02-05 23:01:34 +0300

    Unwrapping with guard

--- 012-Day.playground/Contents.swift
+++ 012-Day.playground/Contents.swift
@@ -6,10 +6,19 @@ var age: Int? = nil
 /*That doesn’t hold any number – it holds nothing. But if we later learn that age, we can use it:*/
 age = 38
 
-//nwrapping optionals
+//unwrapping optionals
 var name: String? = nil
 if let unwrapped = name {
     print("\(unwrapped.count) letters")
 } else {
     print("Missing name")
+}
+
+//Unwrapping with guard
+func greet(_ name: String?){
+    guard let unwrapped = name else {
+        print("You didn't provide a name")
+        return
+    }
+    print ("Hello, \(unwrapped)!")
 }
