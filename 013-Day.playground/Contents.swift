f1ad98108757187db2e1ef3995508c7f2c227a51
JayJay <jjlangatjose@gmail.com>
2020-02-06 23:59:21 +0300

    Types of Data

--- 013-Day.playground/Contents.swift
+++ 013-Day.playground/Contents.swift
@@ -5,3 +5,33 @@ var str = "Hello, playground"
 var name = "Jay Langat"
 name = "Arap"
 let namex = "Constant"
+
+//Types of Data
+var nameY: String
+nameY = "JKL"
+
+var age: Int
+/*nameY = 32*/
+/*nameY = "Thirty Two"*/
+
+var latitude: Double
+latitude = 36.166667
+
+var longitude: Float
+longitude = -86.783333
+longitude = -186.783333
+longitude = -1286.783333
+longitude = -12386.783333
+longitude = -123486.783333
+longitude = -1234586.783333
+
+var stayOutTooLate: Bool
+stayOutTooLate = true
+
+var nothingInBrain: Bool
+nothingInBrain = true
+
+var missABeat: Bool
+missABeat = false
+
+var name: String = "JayJay Langat Arap"
