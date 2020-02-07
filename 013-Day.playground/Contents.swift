3d8f7b88ee11f6b0d90ad2be91a22df31a92aa7d
JayJay <jjlangatjose@gmail.com>
2020-02-07 23:57:35 +0300

    Operators

--- 013-Day.playground/Contents.swift
+++ 013-Day.playground/Contents.swift
@@ -34,4 +34,38 @@ nothingInBrain = true
 var missABeat: Bool
 missABeat = false
 
-var name: String = "JayJay Langat Arap"
+var nameS: String = "JayJay Langat Arap"
+
+//Operators
+var q = 10
+q = q + 1
+q = q - 1
+q = q * q
+
+var r = 10
+r += 10
+r -= 10
+
+var a = 1.1
+var b = 2.2
+var c = a + b
+
+var name1 = "Tim McGraw"
+var name2 = "Romeo"
+var both = name1 + " and " + name2
+
+c > 3
+c >= 3
+c > 4
+c < 4
+
+var nameD = "Tim McGraw"
+nameD == "Tim McGraw"
+
+var stayOutTooLateX = true
+stayOutTooLateX
+!stayOutTooLateX
+
+var nameY = "Tim McGraw"
+nameY == "Tim McGraw"
+nameY != "Tim McGraw"
