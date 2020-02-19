a3870483b1be54b5e829ce948b2addaa2e5d3a8a
JayJay <jjlangatjose@gmail.com>
2020-02-19 23:44:59 +0300

    Force unwrapping optionals

--- 014-Day.playground/Contents.swift
+++ 014-Day.playground/Contents.swift
@@ -137,3 +137,22 @@ func position(of string: String, in array: [String]) -> Int {
 
     return 0
 }
+
+//Force unwrapping optionals
+func yearAlbumReleased(name: String) -> Int? {
+    if name == "Taylor Swift" { return 2006 }
+    if name == "Fearless" { return 2008 }
+    if name == "Speak Now" { return 2010 }
+    if name == "Red" { return 2012 }
+    if name == "1989" { return 2014 }
+
+    return nil
+}
+
+var year = yearAlbumReleased(name: "Red")
+
+if year == nil {
+    print("There was an error")
+} else {
+    print("It was released in \(year)")
+}
