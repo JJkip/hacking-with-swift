dea357fba30e3f561732d957a3ed1bde563da48f
JayJay <jjlangatjose@gmail.com>
2020-02-20 23:37:44 +0300

    Implicitly unwrapped optionals

--- 014-Day.playground/Contents.swift
+++ 014-Day.playground/Contents.swift
@@ -139,7 +139,7 @@ func position(of string: String, in array: [String]) -> Int {
 }
 
 //Force unwrapping optionals
-func yearAlbumReleased(name: String) -> Int? {
+func yearAlbumReleasedX(name: String) -> Int? {
     if name == "Taylor Swift" { return 2006 }
     if name == "Fearless" { return 2008 }
     if name == "Speak Now" { return 2010 }
@@ -149,10 +149,18 @@ func yearAlbumReleased(name: String) -> Int? {
     return nil
 }
 
-var year = yearAlbumReleased(name: "Red")
+var year = yearAlbumReleasedX(name: "Red")
 
 if year == nil {
     print("There was an error")
 } else {
     print("It was released in \(year)")
 }
+//Implicitly unwrapped optionals
+/*
+
+ A regular variable must contain a value. Example: String must contain a string, even if that string is empty, i.e. "". It cannot be nil.
+ An optional variable might contain a value, or might not. It must be unwrapped before it is used. Example: String? might contain a string, or it might contain nil. The only way to find out is to unwrap it.
+ An implicitly unwrapped optional might contain a value, or might not. But it does not need to be unwrapped before it is used. Swift won't check for you, so you need to be extra careful. Example: String! might contain a string, or it might contain nil – and it's down to you to use it appropriately. It’s like a regular optional, but Swift lets you access the value directly without the unwrapping safety. If you try to do it, it means you know there’s a value there – but if you’re wrong your app will crash.
+
+ */
