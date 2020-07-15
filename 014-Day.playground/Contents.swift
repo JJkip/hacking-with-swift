83d6f21e0214c9401810722903e76648df49aea0
JayJay <jjlangatjose@gmail.com>
2020-07-15 23:21:56 +0300

    Properties and property oberservers

--- 014-Day.playground/Contents.swift
+++ 014-Day.playground/Contents.swift
@@ -290,3 +290,24 @@ taylorY.age
 taylorY.sing()
 taylorX.sing()
 taylorZ.sing()
+
+
+//Properties and property oberservers
+struct PersonA {
+    var clothes: String{
+        willSet{
+            updateUI(msg: "I'm changeing from \(clothes) to \(newValue)")
+        }
+        didSet{
+            updateUI(msg: "I just changed from \(oldValue) to \(clothes)")
+        }
+    }
+}
+
+func updateUI(msg: String){
+    print(msg)
+}
+
+var taylorA = PersonA(clothes: "T-shirt")
+taylorA.clothes = "short skirts"
+
