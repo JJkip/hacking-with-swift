f32d3ab455959a402009f7358b86e2255a40527b
JayJay <jjlangatjose@gmail.com>
2020-07-15 23:03:56 +0300

    Class inheritance

--- 014-Day.playground/Contents.swift
+++ 014-Day.playground/Contents.swift
@@ -240,7 +240,7 @@ print(taylorCopy)
 
 //Classes
 
-class Person {
+class PersonX {
     var clothes: String
     var shoes: String
     
@@ -249,3 +249,44 @@ class Person {
         self.shoes = xshoes
     }
 }
+
+class Singer{
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    func sing(){
+        print("La la la la")
+    }
+}
+
+class CountrySinger: Singer{
+    override func sing(){
+        print("Trucks, guitars, and liquor")
+    }
+}
+
+class HeavyMetalSinger: Singer{
+    var noiseLevel: Int
+    
+    init(name: String, age: Int, noiseLevel: Int) {
+        self.noiseLevel = noiseLevel
+        super.init(name: name, age: age)
+    }
+    
+    override func sing() {
+        print("Grrrrr rargh rargh rarrrrrgh")
+    }
+}
+var taylorY = Singer(name: "Taylor", age: 25)
+var taylorX = CountrySinger(name: "Country Taylor", age: 30)
+var taylorZ = HeavyMetalSinger(name: "Taylor Heavy", age: 40, noiseLevel: 100)
+taylorY.name
+taylorY.age
+taylorY.sing()
+taylorX.sing()
+taylorZ.sing()
