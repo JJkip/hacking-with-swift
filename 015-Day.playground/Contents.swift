48fbc79a3b530267a136192e8405c4f74e4d7f2a
JayJay <jjlangatjose@gmail.com>
2020-07-17 23:51:35 +0300

    Polymorphism and typecasting

--- 015-Day.playground/Contents.swift
+++ 015-Day.playground/Contents.swift
@@ -63,3 +63,68 @@ class TylorFanC {
     }
 }
 
+//Polymorphism and typecasting
+
+class Album{
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+    func getPerformance() -> String {
+        return "The album \(name) sold lots"
+    }
+}
+class StudioAlbum: Album{
+    var studio: String
+    init(name: String,studio: String){
+        self.studio = studio
+        super.init(name: name)
+    }
+    override func getPerformance() -> String {
+        return "The studio albmum \(name) sold lots"
+    }
+}
+class LiveAlbum: Album {
+    var location: String
+    init(name: String, location: String){
+        self.location = location
+        super.init(name: name)
+    }
+    override func getPerformance() -> String {
+        return "The live album \(name) sold lots"
+    }
+}
+var taylorSwift = StudioAlbum(name: "Taylor Swift", studio: "The castles studios")
+var fearless = StudioAlbum(name: "Speak Now", studio: "Aimeed Studio ")
+var iTunesLive =  LiveAlbum(name: "iTunes Live from SoHo", location: "New York")
+
+//var allAlbums: [Album] = [taylorSwift, fearless, iTunesLive]
+
+//for album in allAlbums {
+//    print(album.getPerformance())
+//    if let studioAlbum = album as? StudioAlbum{
+//        print(studioAlbum.studio)
+//    } else if let liveAlbum = album as? LiveAlbum{
+//        print(liveAlbum.location)
+//    }
+//}
+
+var allAlbums: [Album] = [taylorSwift, fearless]
+//for album in allAlbums{
+//    let studioAlbum = album as! StudioAlbum
+//    print(studioAlbum.studio)
+//}
+
+//for album in allAlbums as! [StudioAlbum]{
+//    print(album.studio)
+//}
+
+for album in allAlbums as? [LiveAlbum] ?? [LiveAlbum](){
+    print(album.location)
+}
+
+//Converting common types with initializers
+let  number = 5
+//let text = number as! String
+let text = String(number)
+print(text)
