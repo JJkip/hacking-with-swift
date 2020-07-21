029b5d03433f400f52d413825f62e35c70282fea
JayJay <jjlangatjose@gmail.com>
2020-07-21 22:33:39 +0300

    Protocol extensions

--- 015-Day.playground/Contents.swift
+++ 015-Day.playground/Contents.swift
@@ -198,3 +198,47 @@ extension String{
 }
 
 stringWithWhiteSpaces.trim()
+
+
+
+//Protocol extensions
+
+extension Int{
+    func clamp(low: Int, high: Int) -> Int{
+        if (self > high){
+            //if we are higher than the upper bound, return the upper bound
+            return high
+        } else if (self < low) {
+            //if we are lowe than the upper bound, return the lower bound
+            return low
+        }
+        return self
+    }
+}
+let i: Int = 8
+print(i.clamp(low: 0, high: 5))
+
+extension BinaryInteger {
+    func clamp(low: Self, high: Self) -> Self{
+        if(self > high){
+            return high
+        } else if (self < low){
+            return low
+        }
+        return self
+    }
+}
+/*As an example, we might define an extension for our Employee protocol so that all conforming types automatically get a doWork() method:
+
+*/
+protocol EmployeeC {
+    var name: String {get set}
+    var jobTitle: String { get set}
+    func doWork()
+}
+
+extension Employee{
+    func doWork(){
+        print("I'm busy!")
+    }
+}
