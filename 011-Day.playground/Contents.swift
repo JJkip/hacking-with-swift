ae6dbe8e8724fb83a079124574538cd78385abe2
JayJay <jjlangatjose@gmail.com>
2020-07-25 00:17:13 +0300

    Protocal class inhetitance

--- 011-Day.playground/Contents.swift
+++ 011-Day.playground/Contents.swift
@@ -28,6 +28,20 @@ struct Person: FullyNamed {
 }
 let john = Person(fullName: "John Appleseed")
 
+class Starship: FullyNamed{
+    var prefix: String?
+    var name: String
+    init(name: String, prefix: String? = nil) {
+        self.name = name
+        self.prefix = prefix
+    }
+    var fullName: String{
+        return (prefix != nil ? prefix! + " " : "") + name
+    }
+}
+var ncc1701 = Starship(name: "Enterprise", prefix: "USS")
+ncc1701.fullName
+
 protocol Payable{
     func calculateWages() -> Int
 }
