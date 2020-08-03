3810e60acb6aaac4a9a2e2678b85f5855a5d6b1d
JayJay <jjlangatjose@gmail.com>
2020-08-03 23:32:53 +0300

    Protocal Delegation Example with inheritance

--- 011-Day.playground/Contents.swift
+++ 011-Day.playground/Contents.swift
@@ -100,7 +100,9 @@ protocol SomeProtocol_B{
     init()
 }
 class SomeSuperClass_B{
-    init()
+    init() {
+        
+    }
 }
 class SomeSubCLass_B: SomeSuperClass_B, SomeProtocol_B{
     // "required" from SomeProtocol conformance; "override" from SomeSuperClass
@@ -110,17 +112,22 @@ class SomeSubCLass_B: SomeSuperClass_B, SomeProtocol_B{
 }
 
 //Protocols as Types
-class Dice{
+class Dice {
     let sides: Int
-    let generator: RandomNumberGenerator
-    init (sides: Int, generator: RandomNumberGenerator){
+    var generator: RandomNumberGenerator
+    init(sides: Int, generator: RandomNumberGenerator) {
         self.sides = sides
         self.generator = generator
     }
     func roll() -> Int {
-        return Int(generator.random() * Double(sides)) + 1
+//        return Int(generator.random() * Double(sides)) + 1
+        return Int(Double(generator.next()) * Double(sides)) + 1
     }
 }
+//var d6 = Dice(sides: 6, generator:  LinearCongruentialGenerator())
+//for _ in 1...5 {
+//    print("Random Dice roll is \(d6.roll())")
+//}
 
 //Protocal Delegation
 protocol DiceGame{
@@ -130,9 +137,39 @@ protocol DiceGame{
 protocol DiceGameDelegate: AnyObject{
     func gameDidStart(_ game: DiceGame)
     func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int)
-    func gaameDidEnd(_ game: DiceGame)
+    func gameDidEnd(_ game: DiceGame)
 }
 
+class SnakesAndLadders: DiceGame {
+    let finalSquare = 25
+    let dice = Dice(sides: 6, generator: LinearCongruentialGenerator())
+    var square = 0
+    var board: [Int]
+    init() {
+        board = Array(repeating: 0, count: finalSquare + 1)
+        board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+        board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+    }
+    weak var delegate: DiceGameDelegate?
+    func play() {
+        square = 0
+        delegate?.gameDidStart(self)
+        gameLoop: while square != finalSquare {
+            let diceRoll = dice.roll()
+            delegate?.game(self, didStartNewTurnWithDiceRoll: diceRoll)
+            switch square + diceRoll {
+            case finalSquare:
+                break gameLoop
+            case let newSquare where newSquare > finalSquare:
+                continue gameLoop
+            default:
+                square += diceRoll
+                square += board[square]
+            }
+        }
+        delegate?.gameDidEnd(self)
+    }
+}
 
 protocol Payable{
     func calculateWages() -> Int
