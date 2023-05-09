1d4bb5b7f6019c05eb79d5b9204a57b69c798628
jjkip <jjlangatjose@gmail.com>
2023-05-09 23:32:28 +0300

    How to create your own classes

--- 010-Day.playground/Contents.swift
+++ 010-Day.playground/Contents.swift
@@ -91,7 +91,16 @@ print(yammy.name)
 
 
 
-
+//How to create your own classes
+class Game {
+    var score = 0 {
+        didSet {
+            print("score is now \(score)")
+        }
+    }
+}
+var newGame = Game()
+newGame.score += 10
 
 
 
