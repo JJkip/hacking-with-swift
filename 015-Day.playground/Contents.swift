a81996ab62b7516c6449826fecc6c9cc995900e6
jjkip <jjlangatjose@gmail.com>
2023-05-12 23:30:37 +0300

    How to create and use protocols

--- 015-Day.playground/Contents.swift
+++ 015-Day.playground/Contents.swift
@@ -242,3 +242,58 @@ extension Employee{
         print("I'm busy!")
     }
 }
+
+
+//UPDATES
+//How to create and use protocols
+protocol Vehicle {
+    var name: String { get }
+    var currentPassangers: Int { get set }
+    func estimateTime(for distance: Int) -> Int
+    func travel(distance: Int)
+}
+struct Car: Vehicle {
+    let name = "Car"
+    var currentPassangers = 2
+    func estimateTime(for distance: Int) -> Int {
+        distance / 50
+    }
+    func travel(distance: Int) {
+        print("I'm driving \(distance)km")
+    }
+    func openSunroof() {
+        print("It's a nice day")
+    }
+}
+
+func commute(distance: Int, using vehicle: Vehicle) {
+    if vehicle.estimateTime(for: distance) > 100 {
+        print("Too slow try a different vehicle")
+    } else {
+        vehicle.travel(distance: distance)
+    }
+}
+func getTravelEstimate(using vehicles: [Vehicle], distance: Int) {
+    for vehicle in vehicles {
+        let estimate = vehicle.estimateTime(for: distance)
+        print("\(vehicle.name): \(estimate) hours to travel \(distance)")
+    }
+}
+
+let car = Car()
+commute(distance: 100, using: car)
+
+struct Bicycle: Vehicle {
+    let name = "Hero Jet"
+    var currentPassangers = 1
+    func estimateTime(for distance: Int) -> Int {
+        distance / 10
+    }
+    func travel(distance: Int) {
+        print("I'm cycling \(distance)km")
+    }
+}
+let bike = Bicycle()
+commute(distance: 50, using: bike)
+
+getTravelEstimate(using: [car, bike], distance: 150)
