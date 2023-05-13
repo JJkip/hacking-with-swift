ead08d7e0f2f3e9c89707bd76152847d5c59046a
jjkip <jjlangatjose@gmail.com>
2023-05-13 23:57:58 +0300

    How to create and use extensions

--- 015-Day.playground/Contents.swift
+++ 015-Day.playground/Contents.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+//import Cocoa
 
 //Properties and property oberservers
 struct PersonA {
@@ -192,12 +192,12 @@ myIntB
 var stringWithWhiteSpaces = "s w i f t    i o s"
 
 extension String{
-    mutating func trim() {
+    mutating func trimA() {
         self = trimmingCharacters(in: .whitespacesAndNewlines)
     }
 }
 
-stringWithWhiteSpaces.trim()
+stringWithWhiteSpaces.trimA()
 
 
 
@@ -307,3 +307,43 @@ func getRandomBool() -> some Equatable {
     Bool.random()
 }
 print(getRandomNumber() == getRandomNumber())
+
+//How to create and use extensions
+var quote = "   The truth is rarely pure and never simple   "
+let trimmed = quote.trimmingCharacters(in: .whitespacesAndNewlines)
+
+extension String {
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+
+    mutating func trim() {
+        self = self.trimmed()
+    }
+    
+    var lines: [String] {
+        self.components(separatedBy: .whitespacesAndNewlines)
+    }
+}
+quote.trim()
+let lyrics = """
+But I keep cruising
+Can't stop, won't stop moving
+It's like I got this music in my mind
+Saying it's gonna be alright
+"""
+print(lyrics.lines.count)
+
+struct Book {
+    let title: String
+    let pageCount: Int
+    let readingHours: Int
+}
+extension Book {
+    init(title: String, pageCount: Int) {
+        self.title = title
+        self.pageCount = pageCount
+        self.readingHours = pageCount / 50
+    }
+}
+let lotr = Book(title: "Lord of the rings", pageCount: 1178, readingHours: 24)
